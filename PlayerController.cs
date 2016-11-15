e782a799ab497bc89b1409b6a502390000b2aaf8
brettsschmidt <brettsschmidt@gmail.com>
2016-11-15 12:43:38 -0500

    Add files via upload
    Initial code used to script my Spickle game.

---
+++ PlayerController.cs
@@ -0,0 +1,182 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public float moveSpeed = 5;
+    public bool isRunning;
+
+    private Animator anim;
+    private Rigidbody2D myRigidbody;
+    private Renderer myRenderer;
+
+    public bool playerExists;
+
+    private bool playerMoving;
+    public Vector2 lastMove;
+
+    public Rigidbody2D rocket;
+    public float speed = 10f;
+
+    public int startingHealth = 50;                            // The amount of health the player starts the game with.
+    public int currentHealth;                                   // The current health the player has.
+    public float timeBetweenAttacks = 0.5f;     // The time in seconds between each attack.
+    public int attackDamage = 10;               // The amount of health taken away per attack.
+    bool playerInRange;
+    public float timer;
+    public Collider2D enemy;
+
+    public bool playerDead;
+
+    void OnTriggerEnter2D (Collider2D other)
+    {
+        if (other.gameObject.tag  == "enemy") {
+            playerInRange = true;
+            
+            
+        }
+
+
+    }
+
+    void OnTriggerExit2D (Collider2D other)
+    {
+        playerInRange = false;
+    }
+
+
+
+    // Use this for initialization
+    void Start()
+    {
+        
+        anim = GetComponent<Animator>();
+        myRigidbody = GetComponent<Rigidbody2D>();
+        rocket = GetComponent<Rigidbody2D>();
+        myRenderer = GetComponent<Renderer>();
+
+        if (playerExists == true)
+        {
+           playerExists = true;
+            DontDestroyOnLoad(transform.gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+        currentHealth = startingHealth;
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        playerMoving = false;
+        if (playerDead == false)
+        {
+            if (Input.GetAxisRaw("Horizontal") > 0.5f || Input.GetAxisRaw("Horizontal") < -0.5f)
+            {
+                //transform.Translate (new Vector3(Input.GetAxisRaw("Horizontal") * moveSpeed * Time.deltaTime, 0f, 0f));
+                myRigidbody.velocity =
+                    new Vector2(Input.GetAxisRaw("Horizontal") * moveSpeed, myRigidbody.velocity.y);
+                playerMoving = true;
+                lastMove = new Vector2(Input.GetAxisRaw("Horizontal"), 0f);
+            }
+
+            if (Input.GetAxisRaw("Vertical") > 0.5f || Input.GetAxisRaw("Vertical") < -0.5f)
+            {
+                //transform.Translate (new Vector3(0f, Input.GetAxisRaw("Vertical") * moveSpeed * Time.deltaTime, 0f));
+                myRigidbody.velocity =
+                    new Vector2(myRigidbody.velocity.x, Input.GetAxisRaw("Vertical") * moveSpeed);
+                playerMoving = true;
+                lastMove = new Vector2(0f, Input.GetAxisRaw("Vertical"));
+            }
+
+            if (Input.GetAxisRaw("Horizontal") < 0.5f && Input.GetAxisRaw("Horizontal") > -0.5f)
+            {
+                myRigidbody.velocity = new Vector2(0f, myRigidbody.velocity.y);
+            }
+
+            if (Input.GetAxisRaw("Vertical") < 0.5f && Input.GetAxisRaw("Vertical") > -0.5f)
+            {
+                myRigidbody.velocity = new Vector2(myRigidbody.velocity.x, 0f);
+            }
+        }
+
+        anim.SetFloat("MoveX", Input.GetAxisRaw("Horizontal"));
+        anim.SetFloat("MoveY", Input.GetAxisRaw("Vertical"));
+
+        anim.SetBool("PlayerMoving", playerMoving);
+        anim.SetFloat("LastMoveX", lastMove.x);
+        anim.SetFloat("LastMoveY", lastMove.y);
+        anim.SetBool("PlayerDead", playerDead);
+
+        //if (Input.GetMouseButtonDown(0))
+        {
+            //FireRocket();
+            //Rigidbody2D rocketClone = (Rigidbody2D)Instantiate(rocket);
+            //rocketClone.velocity = new Vector2(Input.mousePosition.x, Input.mousePosition.y) * speed;
+        }
+
+        timer += Time.deltaTime;
+
+        // If the timer exceeds the time between attacks, the player is in range and this enemy is alive...
+        if (timer >= .5F)
+        {
+            if (playerInRange == true) //&& enemyHealth.currentHealth > 0)
+            {
+                // ... attack.
+                TakeDamage();
+                timer = 0f;
+                myRenderer.material.color = Color.red;
+
+            }
+
+            // If the player has zero or less health...
+            if (currentHealth <= 0)
+            {
+                // ... tell the animator the player is dead.
+                //Destroy(gameObject);
+                playerDead = true;
+                
+            }
+        }
+        if (timer >= 1f)
+        {
+            myRenderer.material.color = Color.white;
+        }
+
+    }
+    
+   
+
+    public void TakeDamage()
+    {
+        currentHealth -= attackDamage; 
+    }
+
+
+    //void OnTriggerEnter2D(Collider2D other)
+    //{
+    // Destroy(gameObject);
+    //}
+
+    //void FireRocket()
+    //{
+    //Rigidbody2D rocketClone = (Rigidbody2D)Instantiate(rocket);
+    //Vector2 target = Camera.main.ScreenToWorldPoint(new Vector2(Input.mousePosition.x, Input.mousePosition.y));
+    //Vector2 myPos = new Vector2(transform.position.x, transform.position.y + 1);
+    //Vector2 direction = target - myPos;
+    //direction.Normalize();
+    //Quaternion rotation = Quaternion.Euler(0, 0, Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg + 90);
+    //rocketClone.velocity = direction * speed;
+    //rocketClone.velocity = new Vector2(Input.mousePosition.x, Input.mousePosition.y) * speed;
+    // }
+
+   
+
+} 
+
---
+++ PlayerFollow.cs
@@ -0,0 +1,203 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerFollow : MonoBehaviour {
+
+    public Transform target;
+
+    private Animator anim;
+    private Vector3 targetPosition;
+    public float moveSpeed;
+    public float range;
+    public GameObject player;
+    
+    bool playerInRange;
+    PlayerHealth playerHealth;
+    public int attackDamage = 10;
+    public int startingHealth = 50;                            // The amount of health the player starts the game with.
+    public int currentHealth;                                   // The current health the player has.
+    public float timeBetweenAttacks = 0.5f;     // The time in seconds between each attack.
+  
+    public float timer;
+    public Collider2D enemy;
+
+    private static bool cameraExists;
+    public Transform myTransform;
+    public bool playerMoving;
+    public float moveX;
+    public float moveY;
+    public float moveX1;
+    public float moveY1;
+    public float lastmoveX;
+    public float lastmoveY;
+    public float angle;
+
+    public bool sealDead;
+    private Renderer sealRenderer;
+
+    
+    
+
+
+
+    public  
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "rocket")
+        {
+            playerInRange = true;
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        playerInRange = false;
+    }
+
+
+
+
+
+    void Awake()
+    {
+        myTransform = transform;
+        currentHealth = startingHealth;
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+        target = GameObject.FindGameObjectWithTag("Player").transform;
+       
+            DontDestroyOnLoad(gameObject);
+
+        anim = GetComponent<Animator>();
+        sealRenderer = GetComponent<Renderer>();
+
+
+    }
+
+  
+
+    // Update is called once per frame
+    void Update()
+    {
+        var distance = Vector3.Distance(myTransform.position, target.position);
+        if (sealDead == false)
+        {
+            if (range >= distance)
+            {
+                targetPosition = new Vector3(target.transform.position.x,
+                    target.transform.position.y, transform.position.z);
+                moveX = myTransform.position.x - target.transform.position.x;
+                moveY = myTransform.position.y - target.transform.position.y;
+
+                playerMoving = true;
+                transform.position = Vector3.Lerp(transform.position, targetPosition,
+                    moveSpeed * Time.deltaTime);
+            }
+        }
+        
+        if (moveY < 0 && angle >= 1 || angle <= -1)
+        {
+            moveY1 = 1;
+            lastmoveY = 1;
+        }
+        if (moveY > 0 && angle >= 1 || angle <= -1)
+        {
+            moveY1 = -1;
+            lastmoveY = -1;
+        }
+        if (angle <= 1 && angle >= -1)
+        {
+            moveY1 = 0;
+        }
+
+        if (range < distance)
+        {
+            moveX = 0;
+            moveY = 0;
+            moveX1 = 0;
+            moveY1 = 0;
+            playerMoving = false;
+        }
+        angle = moveY / moveX;
+        if(angle < 1 && angle > -1 && moveX > 0)
+        {
+            moveX1 = 1;
+            lastmoveX = 1;
+        }
+        if(angle >= 1 || angle <= -1)
+        {
+            moveX1 = 0;
+        }
+        if(angle > -1 && angle <1 && moveX < 0)
+        {
+            moveX1 = -1;
+            lastmoveX = -1;
+        }
+
+
+       
+        //if (moveX = 1,1)
+        //{
+          //  Debug.LogError("Itworks!!!");
+        //}
+
+
+        timer += Time.deltaTime;
+
+        // If the timer exceeds the time between attacks, the player is in range and this enemy is alive...
+        if (timer >= .5F)
+        {
+            if (playerInRange == true) //&& enemyHealth.currentHealth > 0)
+            {
+                // ... attack.
+                TakeDamage();
+                timer = 0f;
+                sealRenderer.material.color = Color.red;
+            }
+
+            // If the player has zero or less health...
+            if (currentHealth <= 0)
+            {
+                // ... tell the animator the player is dead.
+                //Destroy(gameObject);
+                sealDead = true;
+
+            }
+        }
+        if (timer >= 1f)
+        {
+            sealRenderer.material.color = Color.white;
+        }
+            anim.SetFloat("MoveX", moveX1);
+            anim.SetFloat("MoveY", moveY1);
+            anim.SetFloat("LastMoveX", lastmoveX);
+            anim.SetFloat("LastMoveY", lastmoveY);
+            anim.SetBool("PlayerMoving", playerMoving);
+            anim.SetBool("SealDead", sealDead);
+           
+        
+
+
+        
+
+
+
+    }
+    void TakeDamage()
+    {
+        currentHealth -= attackDamage;
+    }
+    }
+    
+
+
+//void Attack()
+    //{
+      //  playerHealth.TakeDamage(attackDamage);
+    //}
+
+
